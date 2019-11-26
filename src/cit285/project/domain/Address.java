0b36159970088885bbba40fe6b35a3baca24ac58
sbrannsinger <56648914+sbrannsinger@users.noreply.github.com>
2019-11-26 18:41:14 -0500

    Add files via upload

---
+++ src/cit285/project/domain/Address.java
@@ -0,0 +1,70 @@
+package cit285.project.domain;
+
+public class Address {
+	private int addressId;
+	private int userId;
+	private String street;
+	private String city;
+	private String state;
+	private String zipcode;
+	private String country;
+	
+	
+	public int getAddressId() {
+		return addressId;
+	}
+
+	public void setAddressId(int addressId) {
+		this.addressId = addressId;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZipcode() {
+		return zipcode;
+	}
+
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	
+}
