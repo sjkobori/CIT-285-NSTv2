ddfd76c6f24a5e732ef61028b47470915abf9c71
sbrannsinger <sbrannsinger@gmail.com>
2019-12-04 11:08:51 -0500

    commit

--- src/cit285/project/presentation/web/BookServlet.java
+++ src/cit285/project/presentation/web/BookServlet.java
@@ -21,61 +21,65 @@ import cit285.project.services.BookServicesAPI;
 public class BookServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private BookServicesAPI bookServices;
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public BookServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-    
-    @Override
-    public void init() throws ServletException {
-    	try{
-			//System.out.println("Configuring services...");
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public BookServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void init() throws ServletException {
+		try {
+			// System.out.println("Configuring services...");
 			BookSystemConfig.configureServices();
+		} catch (Exception e) {
 		}
-		catch(Exception e){}
-		//System.out.println("Getting payments services...");
+		// System.out.println("Getting payments services...");
 		bookServices = BookSystemConfig.getBookServices();
 	}
-    
+
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
+
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
-	
+
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		//doGet(request, response);
-		
+		// doGet(request, response);
+
 		HttpSession session = request.getSession();
 		String source = request.getParameter("source");
-		
-		if (source.equals("login")){
+
+		if (source.equals("login")) {
 			ArrayList<Book> books = null;
-			
+
 			books = bookServices.getBooks();
-			
+
 			// Add attribute to the session
 			session.setAttribute("books", books);
-			
+
 			getServletContext().getRequestDispatcher("/WEB-INF/jsp/booklist.jsp").forward(request, response);
 		} else if (source.equals("inspectBook") || source.equals("cart")) {
 			// Add attribute to the session
-						//session.setAttribute("books", books);
-						
-						getServletContext().getRequestDispatcher("/WEB-INF/jsp/booklist.jsp").forward(request, response);
-		}
-		else{
-			session.setAttribute("Error","Unknown source!");
+			// session.setAttribute("books", books);
+
+			getServletContext().getRequestDispatcher("/WEB-INF/jsp/booklist.jsp").forward(request, response);
+		} else {
+			session.setAttribute("Error", "Unknown source!");
 			getServletContext().getRequestDispatcher("/WEB-INF/jsp/error.jsp").forward(request, response);
 		}
 	}
